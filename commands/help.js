3d2d8747f354b2ee0fd74406cd5ce7ba79e8c6ac
vnloading99 <contact.loading99@gmail.com>
2024-05-14 13:14:03 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -26,7 +26,7 @@ const { ButtonStyle } = require('discord.js');
 
 module.exports = {
   name: "help",
-  description: "Get information about bot and commands.",
+  description: "Nhận thông tin về bot và các lệnh.",
   permissions: "0x0000000000000800",
   options: [],
 
@@ -34,48 +34,48 @@ module.exports = {
     try {
       const musicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('🎸 **Music Commands**')
+        .setTitle('🎸 **Lệnh**')
         .addFields(
-          { name: '🎹 Play', value: 'Stream a song from a given link or text from sources' },
-          { name: '⏹️ Stop', value: 'Makes the bot stop playing music and leave the voice' },
-          { name: '📊 Queue', value: 'View and manage the song queue of this server' },
-          { name: '⏭️ Skip', value: 'Skip the current playing song' },
-          { name: '⏸️ Pause', value: 'Pause the currently playing song' },
-          { name: '▶️ Resume', value: 'Resume the current paused song' },
-          { name: '🔁 Loop', value: 'Toggle loop mode for queue and current song' },
-          { name: '🔄 Autoplay', value: 'Enable or disable autoplay [play random songs ]' },
-          { name: '⏩ Seek', value: 'Seek to a specific time in the current song' },
-          { name: '⏮️ Previous', value: 'Play the previous song in the queue' },
-          { name: '🔀 Shuffle', value: 'Shuffle the songs in queue' },
-          { name: '📃 playlist', value: 'manage the playlists' }
+          { name: '🎹 Play', value: 'Phát một bài hát từ một liên kết hoặc văn bản được cung cấp từ các nguồn.' },
+          { name: '⏹️ Stop', value: 'Dừng phát nhạc và rời khỏi kênh thoại.' },
+          { name: '📊 Queue', value: 'Xem và quản lý hàng đợi bài hát của máy chủ này.' },
+          { name: '⏭️ Skip', value: 'Bỏ qua bài hát đang phát hiện tại.' },
+          { name: '⏸️ Pause', value: 'Tạm dừng bài hát đang phát hiện tại.' },
+          { name: '▶️ Resume', value: 'Tiếp tục phát bài hát đã tạm dừng.' },
+          { name: '🔁 Loop', value: 'Bật/Tắt chế độ lặp lại cho hàng đợi và bài hát hiện tại.' },
+          { name: '🔄 Autoplay', value: 'Bật hoặc tắt chế độ tự động phát (phát ngẫu nhiên các bài hát)].' },
+          { name: '⏩ Seek', value: 'Di chuyển đến một thời điểm cụ thể trong bài hát hiện tại.' },
+          { name: '⏮️ Previous', value: 'Phát bài hát trước đó trong hàng đợi.' },
+          { name: '🔀 Shuffle', value: 'Xáo trộn các bài hát trong hàng đợi.' },
+          { name: '📃 playlist', value: 'Quản lý danh sách phát' }
         )
-        .setImage(`https://cdn.discordapp.com/attachments/1004341381784944703/1165201249331855380/RainbowLine.gif?ex=654f37ba&is=653cc2ba&hm=648a2e070fab36155f4171962e9c3bcef94857aca3987a181634837231500177&`); 
+        .setImage(`https://cdn.discordapp.com/attachments/1235520801185337346/1237021270126624808/2024.png?ex=66440495&is=6642b315&hm=84b2772d1fca2179f3bc3532123c93ba127ef5f84c3171262509052bed991236&`); 
 
       const basicCommandsEmbed = new EmbedBuilder()
         .setColor(client.config.embedColor)
-        .setTitle('✨ **Basic Commands**')
+        .setTitle('✨ **Lệnh Cơ Bản**')
         .addFields(
-          { name: '🏓 Ping', value: "Check the bot's latency" },
-          { name: '🗑️ Clear', value: 'Clear the song queue of this server' },
-          { name: '⏱️ Time', value: 'Display the current song playback time' },
-          { name: '🎧 Filter', value: 'Apply filters to enhance the sound as you love' },
-           { name: '🎵 Now Playing', value: 'Display the currently playing song information' },
-          { name: '🔊 Volume', value: 'Adjust the music volume [ hearing at high volumes is risky ]' },
+          { name: '🏓 Ping', value: "Kiểm tra độ trễ của bot." },
+          { name: '🗑️ Clear', value: 'Xóa sạch hàng đợi bài hát của máy chủ này.' },
+          { name: '⏱️ Time', value: 'Hiển thị thời gian phát hiện tại của bài hát.' },
+          { name: '🎧 Filter', value: 'Áp dụng bộ lọc để cải thiện âm thanh theo ý thích của bạn.' },
+           { name: '🎵 Now Playing', value: 'Hiển thị thông tin về bài hát đang phát hiện tại.' },
+          { name: '🔊 Volume', value: 'Điều chỉnh âm lượng nhạc [nghe ở âm lượng cao có nguy cơ].' },
         ) 
-       .setImage('https://cdn.discordapp.com/attachments/1150827819547504741/1168917372267151370/standard.gif?ex=65538222&is=65410d22&hm=b4994392f44679da41fc9304eb69deaa3769e136057556deec0db69ae8d33a97&')
+       .setImage('https://cdn.discordapp.com/attachments/1235520801185337346/1237021270126624808/2024.png?ex=66440495&is=6642b315&hm=84b2772d1fca2179f3bc3532123c93ba127ef5f84c3171262509052bed991236&')
       const button1 = new ButtonBuilder()
-        .setLabel('YouTube')
-        .setURL('https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A')
+        .setLabel('Discord')
+        .setURL('https://discord.gg/loading99')
         .setStyle(ButtonStyle.Link);
 
       const button2 = new ButtonBuilder()
         .setLabel('Discord')
-        .setURL('https://discord.gg/FUEHs7RCqz')
+        .setURL('https://discord.gg/loading99')
         .setStyle(ButtonStyle.Link);
 
       const button3 = new ButtonBuilder()
-        .setLabel('Code')
-        .setURL('https://github.com/GlaceYT/MUSIC-BOT-v3.6')
+        .setLabel('Discord')
+        .setURL('https://discord.gg/loading99')
         .setStyle(ButtonStyle.Link);
 
       const row = new ActionRowBuilder()
