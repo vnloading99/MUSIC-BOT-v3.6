77f839d36e01e490d3316328f6f2e88ee0ed438a
vnloading99 <contact.loading99@gmail.com>
2024-05-14 13:19:21 +0700

    Update nowplaying.js

--- commands/nowplaying.js
+++ commands/nowplaying.js
@@ -9,21 +9,21 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Không có nhạc đang phát!!`, ephemeral: true }).catch(e => { })
 
       const track = queue.songs[0];
-      if (!track) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!track) return interaction.reply({ content: `⚠️ Không có nhạc đang phát!!`, ephemeral: true }).catch(e => { })
 
       const embed = new EmbedBuilder();
       embed.setColor(client.config.embedColor);
       embed.setThumbnail(track.thumbnail);
       embed.setTitle(track.name)
-      embed.setDescription(`> **Audio** \`%${queue.volume}\`
-> **Duration :** \`${track.formattedDuration}\`
-> **URL :** **${track.url}**
-> **Loop Mode :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
-> **Filter**: \`${queue.filters.names.join(', ') || 'Off'}\`
-> **By :** <@${track.user.id}>`);
+      embed.setDescription(`> **Âm nhạc:** \`%${queue.volume}\`
+> **Thời lượng :** \`${track.formattedDuration}\`
+> **Địa chỉ URL :** **${track.url}**
+> **Chế độ lặp lại :** \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'Tất cả Hàng đợi' : 'Bài hát này') : 'Off'}\`
+> **Bộ lọc**: \`${queue.filters.names.join(', ') || 'Off'}\`
+> **Bởi :** <@${track.user.id}>`);
 
 
       interaction.reply({ embeds: [embed] }).catch(e => { })
