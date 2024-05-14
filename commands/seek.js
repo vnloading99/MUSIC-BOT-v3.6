0be2c4bb05d504f57017460e736870e652d675ef
vnloading99 <contact.loading99@gmail.com>
2024-05-15 00:08:12 +0700

    Update seek.js

--- commands/seek.js
+++ commands/seek.js
@@ -2,11 +2,11 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "seek",
-  description: "jump to the timestamp",
+  description: "Nhảy đến thời điểm đã chỉ định",
   permissions: "0x0000000000000800",
   options: [{
     name: "time",
-    description: "enter timestamp",
+    description: "Nhập thời điểm",
     type: ApplicationCommandOptionType.String,
     required: true
   }],
@@ -15,13 +15,13 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Không có nhạc đang phát!!`, ephemeral: true }).catch(e => { })
 
       let position = getSeconds(interaction.options.getString("position"))
       if(isNaN(position)) return interaction.reply({ content: `usage : 2:40`, ephemeral: true }).catch(e => { })
 
       queue.seek(position)
-      interaction.reply({ content: `▶️ **Taking you on a time-travel journey to the specified timestamp.**`}).catch(e => { })
+      interaction.reply({ content: `▶️ **Đưa bạn vào một hành trình du hành thời gian đến thời điểm được chỉ định.**`}).catch(e => { })
 
     } catch (e) {
       console.error(e);
