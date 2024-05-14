d990574ccb011da1c10e9eac90fdd114a8bd6bfe
vnloading99 <contact.loading99@gmail.com>
2024-05-14 12:51:00 +0700

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -28,7 +28,7 @@ module.exports = async (client, interaction) => {
 
 try {
 if (!interaction?.guild){
-return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
+return interaction?.reply({ content: "Giới Hạn Tần Suất Hoạt Động.", ephemeral: true })
 } else {
 
     function cmd_loader() {
@@ -62,7 +62,7 @@ return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
 
             if (!channel_filter?.length > 0 && !interaction?.member?.permissions?.has("0x0000000000000020")) {
             channel_filter = data?.channels?.map(x => `<#${x.channel}>`).join(", ")
-            return interaction?.reply({ content: '🔴 Rate Limited'.replace("{channel_filter}", channel_filter), ephemeral: true }).catch(e => { })
+            return interaction?.reply({ content: '🔴 Giới Hạn Tần Suất Hoạt Động'.replace("{channel_filter}", channel_filter), ephemeral: true }).catch(e => { })
             }
         }
             }
@@ -89,21 +89,21 @@ return interaction?.reply({ content: "Rate Limited.", ephemeral: true })
             }
             }
             if (props && props.voiceChannel) {
-            if (!interaction?.member?.voice?.channelId) return interaction?.reply({ content: `🔴 Join Voice channel First!!`, ephemeral: true }).catch(e => { })
+            if (!interaction?.member?.voice?.channelId) return interaction?.reply({ content: `🔴 Hãy tham gia kênh Voice trước!!`, ephemeral: true }).catch(e => { })
             const guild_me = interaction?.guild?.members?.cache?.get(client?.user?.id);
             if (guild_me?.voice?.channelId) {
             if (guild_me?.voice?.channelId !== interaction?.member?.voice?.channelId) {
-            return interaction?.reply({ content: `🔴 Must be in same VC!!`, ephemeral: true }).catch(e => { })
+            return interaction?.reply({ content: `🔴 Phải ở cùng một VC!!`, ephemeral: true }).catch(e => { })
             }
             }
             }
             return props.run(client, interaction);
             
             } else {
-            return interaction?.reply({ content: `▶️ Missing Permissions: **${props?.permissions?.replace("0x0000000000000020", "MANAGE GUILD")?.replace("0x0000000000000800", "SEND MESSAGES") || "SEND MESSAGES"}**`, ephemeral: true });
+            return interaction?.reply({ content: `▶️ Thiếu quyền hạn: **${props?.permissions?.replace("0x0000000000000020", "MANAGE GUILD")?.replace("0x0000000000000800", "SEND MESSAGES") || "SEND MESSAGES"}**`, ephemeral: true });
             }
             } catch (e) {
-            return interaction?.reply({ content: `❌ Error...\n\n\`\`\`${e?.message}\`\`\``, ephemeral: true });
+            return interaction?.reply({ content: `❌ Lỗi...\n\n\`\`\`${e?.message}\`\`\``, ephemeral: true });
             }
             }
             });
